d9f055bcfc0e9c563b79714c9dfe18aa7f1c74d3
guptachunky <chunkygupta95@gmail.com>
2018-02-21 16:21:29 +0530

    question 6

--- src/main/java/Author.java
+++ src/main/java/Author.java
@@ -1,18 +1,24 @@
 import jdk.Exported;
 
+import javax.naming.Name;
 import javax.persistence.*;
 import java.util.Date;
 
-@Entity
+@Entity(name = "author")
 public class Author {
 
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
+
+    @Column(name = ("First_Name"))
     private String firstName;
+    @Column(name = ("Last_Name"))
     private String lastName;
+    @Column(name = ("Age"))
     private int age;
 
     @Temporal(TemporalType.DATE)
+    @Column(name = ("Date_Of_Birth"))
     private Date dob;
 
     public Date getDob() {
